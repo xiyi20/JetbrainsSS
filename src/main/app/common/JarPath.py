b38f2e63c96c410a172528ecd435fd54c8328184
dxcxy <3229429589@qq.com>
2026-09-14 11:34:57 +0800

    fix(app): 修正 PyCharm 253 版本的 jar 路径与 Logo 资源
    - 将 PyCharm 253 版本的 jar 路径从 /lib/app.jar 修正为 /lib/product-backend.jar
    - 将 Logo 资源由 pycharm_core_logo 替换为 pycharm_logo

--- src/main/app/common/JarPath.py
+++ src/main/app/common/JarPath.py
@@ -6,7 +6,7 @@ class JarPath(Enum):
         '253': ["/lib/product-backend.jar", ["idea_logo","idea_logo_subscription"]]
     }
     PyCharm = {
-        '253': ["/lib/app.jar", ["pycharm_core_logo"]]
+        '253': ["/lib/product-backend.jar", ["pycharm_logo"]]
     }
     WebStorm = {
         '253': ["/lib/app-backend.jar", ["artwork/webide_logo"]]
